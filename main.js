349107a16309c81a22dd1aecfb64081b5524f9e3
Jamtes44 <jamesgomez2703@gmail.com>
2025-11-18 12:44:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { OBJLoader } from "https://unpkg.com/three@0.160.0/examples/jsm/loaders/
 // Escena, cámara, renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 1.6, 2);
+camera.position.set(0, 1.6, 5);
 camera.lookAt(new THREE.Vector3(0, 0, -3));
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
